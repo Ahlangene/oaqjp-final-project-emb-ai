4d213fbf1b3f1a63a81985fabb13249749e1ca09
Ahlangene <ndana.ahlangene@gmail.com>
2026-06-01 16:03:32 -0400

    Sever and emotion detector debug

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,5 +6,16 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header)
-    result= json.loads(response.text)
-    return result['emotionPredictions'][0]['emotion']
+    if text_to_analyze=='':
+       emotions = {"anger": None, "disgust": None, "fear": None, "joy": None, "sadness": None} 
+    elif response.status_code == 200:
+		# Parse the response from the API
+		formatted_response = json.loads(response.text)
+		emotions=result['emotionPredictions'][0]['emotion']
+	# If the response status code is 500, set label and score to None
+	elif response.status_code == 500:
+		emotions = None
+	# For any other unexpected status codes, set label and score to None
+	else:
+		emotions = None
+    return emotions
--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app=Flask("Emotion Detection")
 def render_page():
     return render_template("index.html")
 
-@app.route("/detect")
+@app.route("/emotionDetector")
 def display_emotion():
     #Getting the text
     sent=request.args.get("textToAnalyze")
@@ -15,18 +15,24 @@ def display_emotion():
     #Adding the text to the emotion detector function
     result=emotion_detector(sent)
 
+    if label is None:
+		return "Invalid input! Try again."
+    
     # Initial output result
     output="For the given statement, the system response is "
     value=0
     feeling=''
-    for emotion,num in emotion_dict.items():
+    for emotion,num in result.items():
         if not emotion=='sadness':
-            output=output+ emotion+': '+num+', '
+            output=output+ emotion+': '+str(num)+', '
         else:
-            output=output+ emotion+': '+num+'. '
+            output=output+ emotion+': '+str(num)+'. '
         if num>value:
             value=num
             feeling=emotion
 
     output= output+f'The dominant emotion is {feeling}.'
     return output
+
+if __name__ == "__main__": 
+    app.run(host="0.0.0.0", port=5000) 
